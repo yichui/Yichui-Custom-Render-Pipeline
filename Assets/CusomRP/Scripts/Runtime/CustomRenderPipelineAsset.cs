83b47199133e50dfb7efdd0b04f81ae8a8814bf7
yichui <896873144@qq.com>
2023-04-14 04:33:07 +0800

    asset

---
+++ Assets/CusomRP/Scripts/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    //重写创建实际RenderPipeline的函数
+    protected override RenderPipeline CreatePipeline()
+    {
+        //暂时返回null
+        return null;
+    }
+}
