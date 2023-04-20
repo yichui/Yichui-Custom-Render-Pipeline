660cc9d8e6173550364c0429ca241d04f17a7ab8
yichui <896873144@qq.com>
2023-04-21 03:18:52 +0800

    batching

--- Assets/CusomRP/Shaders/UnityInput.hlsl
+++ Assets/CusomRP/Shaders/UnityInput.hlsl
@@ -2,13 +2,21 @@
 #ifndef CUSTOM_UNITY_INPUT_INCLUDED
 #define CUSTOM_UNITY_INPUT_INCLUDED
 
+
+//这三个变量也使用CBUFFER，使用UnityPerDraw命名该Buffer（UnityPerDraw为Unity内置好的名字）
+CBUFFER_START(UnityPerDraw)
 float4x4 unity_ObjectToWorld;//内置着色器变量,HLSLPROGRAM中需要我们自己声明一些获取内置着色器变量
 float4x4 unity_WorldToObject;
-float4x4 unity_MatrixVP;//内置着色器变量,HLSLPROGRAM中需要我们自己声明一些获取内置着色器变量
-
+//在定义（UnityPerDraw）CBuffer时，因为Unity对一组相关数据都归到一个Feature中，即使我们没用到unity_LODFade，我们也需要放到这个CBuffer中来构造一个完整的Feature
+//如果不加这个unity_LODFade，不能支持SRP Batcher
+float4 unity_LODFade;
 real4 unity_WorldTransformParams;
+CBUFFER_END
+
 
 float4x4 unity_MatrixV;
+float4x4 unity_MatrixVP;//内置着色器变量,HLSLPROGRAM中需要我们自己声明一些获取内置着色器变量
+
 float4x4 glstate_matrix_projection;
 
 #endif
--- Assets/CusomRP/Shaders/Unlit.shader
+++ Assets/CusomRP/Shaders/Unlit.shader
@@ -2,11 +2,13 @@ Shader "Custom RP/Unlit"
 {
     Properties
     {
-          _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        //[可选：特性]变量名(Inspector上的文本,类型名) = 默认值
+        //[optional: attribute] name("display text in Inspector", type name) = default value
+        _BaseColor("Color",Color) = (1.0,1.0,1.0,1.0)
     }
+
     SubShader
     {
-
         Pass
         {
             HLSLPROGRAM
